991dcf367a9c632ebbfb77486d987f37f44e4f8f
Oskar Gester <gester.oskar@gmail.com>
2025-02-08 14:00:10 +0100

    set up initial user routes for getUsers signup and login, bare minimum

---
+++ routes/users-routes.js
@@ -0,0 +1,16 @@
+// require = imports in nodeJS
+const express = require("express");
+
+const userControllers = require("../controllers/user-controllers");
+
+const router = express.Router();
+
+// controller functions should not be executed her just pointed at where to execute when
+// a request reaches the route
+router.get("/", userControllers.getUsers);
+
+router.post("/signup", userControllers.signup);
+
+router.post("/login", userControllers.login);
+
+module.exports = router;
